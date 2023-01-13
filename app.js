a6a7cf89570b759403f82512d27f6b8a63e87d0a
ali-yigit <alygtalygt@gmail.com>
2023-01-13 15:41:26 +0300

    rgb

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const btn = document.getElementById("btn");
+
 const body = document.getElementById("body");
+const p = document.createElement("p")
+btn.appendChild(p)
 
 btn.addEventListener("click", (x) => {
   const color1 = Math.floor(Math.random() * 255);
   const color2 = Math.floor(Math.random() * 255);
   const color3 = Math.floor(Math.random() * 255);
-  const colorRgb = `rgb(${color1},${color2},${color3})`;
+  const colorRgb = `Rgb(${color1},${color2},${color3})`;
   body.style.backgroundColor = colorRgb;
   console.log(colorRgb);
+  p.innerText="Color: "+ colorRgb
+  p.style.border="none"
 });
 
